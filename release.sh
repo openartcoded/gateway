96f3c022e74df250d667142831f4d8a8f2974582
Nordine Bittich <nordine@bittich.be>
2022-04-03 12:10:02 +0200

    remove pushing image manually

--- release.sh
+++ release.sh
@@ -14,13 +14,3 @@ echo "release" $releaseVersion ", next" $nextVersion
 mvn --batch-mode -Dtag=$releaseVersion release:prepare \
                  -DreleaseVersion=$releaseVersion \
                  -DdevelopmentVersion=$nextVersion
-
-mvn release:clean
-git pull
-
-git checkout $releaseVersion
-docker build -t nbittich/api-gateway:v$releaseVersion .
-docker tag nbittich/api-gateway:v$releaseVersion nbittich/api-gateway:v$releaseVersion
-docker push nbittich/api-gateway:v$releaseVersion
-
-git checkout main
